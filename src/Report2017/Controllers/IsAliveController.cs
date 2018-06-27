88ca3af2abac5a901f7bf9316b8d6ebe1ab4b9b7
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-06-27 23:09:24 +0300

    added isalive

---
+++ src/Report2017/Controllers/IsAliveController.cs
@@ -0,0 +1,24 @@
+﻿using Lykke.Common.Api.Contract.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Report2017.Controllers
+{
+    [Route("api/[controller]")]
+    public class IsAliveController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new IsAliveResponse
+            {
+                Name = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationName,
+                Version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion,
+#if DEBUG
+                IsDebug = true,
+#else
+                IsDebug = false,
+#endif
+            });
+        }
+    }
+}
